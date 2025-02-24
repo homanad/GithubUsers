94af57688ac4a09811161c2f38a507d85a695599
Man Ho <hmman.ct@gmail.com>
2025-02-24 13:37:20 +0700

    Temp: Show details

--- presentation/src/main/java/com/homanad/android/githubusers/ui/details/DetailsFragment.kt
+++ presentation/src/main/java/com/homanad/android/githubusers/ui/details/DetailsFragment.kt
@@ -1,17 +1,58 @@
 package com.homanad.android.githubusers.ui.details
 
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.navArgs
+import coil3.load
+import com.homanad.android.domain.models.GithubUser
 import com.homanad.android.githubusers.R
 import com.homanad.android.githubusers.common.BaseBindingFragment
 import com.homanad.android.githubusers.databinding.FragmentDetailsBinding
+import com.homanad.android.githubusers.ui.details.vm.DetailsViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class DetailsFragment : BaseBindingFragment<FragmentDetailsBinding>() {
 
     override fun getContentRes() = R.layout.fragment_details
 
+    private val viewModel by viewModels<DetailsViewModel>()
+
     private val args by navArgs<DetailsFragmentArgs>()
 
     override fun setupUI() {
-        println("------------${args.username}")
+        viewModel.sendIntent(DetailsViewModel.Intent.GetUser(args.username))
+    }
+
+    override fun handleUIState() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                viewModel.stateFlow.collectLatest {
+                    handleState(it)
+                }
+            }
+        }
+    }
+
+    private fun handleState(state: DetailsViewModel.State) {
+        when (state) {
+            is DetailsViewModel.State.User -> {
+                showUserData(state.user)
+            }
+        }
+    }
+
+    private fun showUserData(user: GithubUser) {
+        println("-----------user: $user")
+
+        with(binding) {
+            cardUser.imgAvatar.load(user.avatarUrl)
+            cardUser.txtUsername.text = user.username
+            cardUser.txtHtmlUrl.text = user.htmlUrl
+        }
     }
 }
---
+++ presentation/src/main/java/com/homanad/android/githubusers/ui/details/vm/DetailsViewModel.kt
@@ -0,0 +1,37 @@
+package com.homanad.android.githubusers.ui.details.vm
+
+import androidx.lifecycle.viewModelScope
+import com.homanad.android.domain.models.GithubUser
+import com.homanad.android.domain.usecases.github.GetGithubUserUseCase
+import com.homanad.android.githubusers.common.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailsViewModel @Inject constructor(
+    private val getGithubUserUseCase: GetGithubUserUseCase
+) : BaseViewModel<DetailsViewModel.Intent, DetailsViewModel.State>() {
+
+    sealed interface Intent {
+        data class GetUser(val username: String) : Intent
+    }
+
+    sealed interface State {
+        data class User(val user: GithubUser) : State
+    }
+
+    override suspend fun processIntent(intent: Intent) {
+        when (intent) {
+            is Intent.GetUser -> getUser(intent.username)
+        }
+    }
+
+    private fun getUser(username: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val data = getGithubUserUseCase(username)
+            emitState(State.User(data))
+        }
+    }
+}
