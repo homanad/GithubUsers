4d5297d6c3d3bc953f2139598098342bf778bf41
Man Ho <hmman.ct@gmail.com>
2025-02-25 16:38:23 +0700

    Write documents

--- presentation/src/main/java/com/homanad/android/githubusers/mappers/UserDetailsMapper.kt
+++ presentation/src/main/java/com/homanad/android/githubusers/mappers/UserDetailsMapper.kt
@@ -6,6 +6,9 @@ import com.homanad.android.githubusers.R
 import com.homanad.android.githubusers.common.base.BaseMapper
 import com.homanad.android.githubusers.models.UserDetails
 
+/**
+ * This class is used to map data from GithubUser model (Domain layer) to UserDetails model (UI layer).
+ */
 class UserDetailsMapper(
     private val context: Context,
     private val userItemMapper: UserItemMapper
@@ -20,6 +23,9 @@ class UserDetailsMapper(
         )
     }
 
+    /**
+     * This function will convert the follower/following value into a string for display.
+     */
     private fun convertToDisplay(value: Int?): String {
         return when (value) {
             0, null -> "0"
@@ -28,6 +34,14 @@ class UserDetailsMapper(
         }
     }
 
+    /**
+     * This function is used to round a number down to the nearest number,
+     * Depending on its value, the nearest rounded value will change:
+     * - Values from 0 to 999 will be rounded to the nearest ten.
+     * - Values greater than 1000 will be rounded to the nearest hundred.
+     * @see value Value to be rounded.
+     * @see downTo The nearest value will be rounded.
+     */
     private fun convertRoundDown(value: Int, downTo: Int): String {
         val dividedValue = value.toFloat() / downTo
         return if (isInt(dividedValue)) {
